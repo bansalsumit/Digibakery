4d5af86eadf465cec30ba2505f8c2b8344b686d3
Sumit Bansal <sbansal.mait@gmail.com>
2023-08-04 18:26:00 +0530

    Added functionality to marking orders fulfilled.

--- app/components/orders/index.jsx
+++ app/components/orders/index.jsx
@@ -3,23 +3,48 @@ import Table from "./table";
 
 const fetchOrders = async (sortBy) => {
   const params = new URLSearchParams();
-  params.append('sort_by', sortBy);
+  params.append("sort_by", sortBy);
   const url = `/api/orders.json?${params.toString()}`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
 };
 
+const updateOrderStatusToFullFill = async (orderId) => {
+  const url = `/api/orders/${orderId}/fulfill.json?`;
+  const response = await fetch(url, { method: "PUT" });
+  const data = await response.json();
+  return data;
+};
+
 export default () => {
   const [orders, setOrders] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
-  async function fetchOrdersHandler(sortBy='') {
+  async function fetchOrdersHandler(sortBy = "") {
     try {
       setIsLoading(true);
       const orders = await fetchOrders(sortBy);
       setOrders(orders);
       setIsLoading(false);
+    } catch (er) {
+      alert(`uh oh! ${er}`);
+    }
+  }
+
+  async function updateOrderStatusToFullFillhandler(orderId) {
+    try {
+      const updatedOrder = await updateOrderStatusToFullFill(orderId);
+      const ordersList = orders.map((order) => {
+        let newOrder;
+        if (order.id == orderId) {
+          newOrder = updatedOrder;
+        } else {
+          newOrder = order;
+        }
+        return newOrder;
+      });
+      setOrders(ordersList);
     } catch (er) {
       alert(`uh oh! ${er}`);
     }
@@ -31,7 +56,12 @@ export default () => {
 
   return (
     <>
-      <Table orders={orders} isLoading={isLoading} fetchOrdersHandler={fetchOrdersHandler}/>
+      <Table
+        orders={orders}
+        isLoading={isLoading}
+        fetchOrdersHandler={fetchOrdersHandler}
+        rowHandler={updateOrderStatusToFullFillhandler}
+      />
     </>
   );
 };
--- app/components/orders/table.jsx
+++ app/components/orders/table.jsx
@@ -1,4 +1,6 @@
-const OrderTable = ({ orders, isLoading, fetchOrdersHandler }) => {
+import OrderRow from "./orderRow";
+
+const OrderTable = ({ orders, isLoading, fetchOrdersHandler, rowHandler }) => {
   const orderSortingHandler = (sortBy) => {
     fetchOrdersHandler(sortBy);
   };
@@ -26,32 +28,18 @@ const OrderTable = ({ orders, isLoading, fetchOrdersHandler }) => {
         </thead>
         <tbody>
           {!isLoading &&
-            orders.map((order) => <OrderRow key={order.id} order={order} />)}
+            orders.map((order) => (
+              <OrderRow
+                key={order.id}
+                order={order}
+                onClickEvent={rowHandler}
+              />
+            ))}
         </tbody>
       </table>
       {isLoading && <p>Loading...</p>}
     </>
   );
-};
-
-const OrderRow = ({ order }) => {
-  return (
-    <tr>
-      <td>{order.id}</td>
-      <td>{formatDate(order.created_at)}</td>
-      <td>{formatDate(order.pick_up_at)}</td>
-      <td>{order.customer_name}</td>
-      <td>{order.item}</td>
-      <td>{order.quantity}</td>
-      <td>{order.fulfilled ? `Fulfilled` : `In progress`}</td>
-      <td></td>
-    </tr>
-  );
-};
-
-const formatDate = (dateString) => {
-  let date = new Date(dateString);
-  return date.toLocaleDateString();
 };
 
 export default OrderTable;
